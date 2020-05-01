df91010ba099591514cf018fc07a4f826ac00d71
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2020-05-01 10:32:58 +0530

    Added feature to add element to end...

---
+++ list.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+List_ptr create_list(void) {
+  List_ptr list = malloc(sizeof(List));
+  list->head = NULL;
+  list->last = NULL;
+  list->count = 0;
+
+  return list;
+}
+
+Node_ptr create_node(int value) {
+  Node_ptr new_node = malloc(sizeof(Node));
+  new_node->value = value;
+  new_node->next = NULL;
+  return new_node;
+}
+
+Status add_to_end(List_ptr list, int value) { 
+  Node_ptr new_node = create_node(value);
+  if (new_node == NULL) {
+    return Failure;
+  }
+  if (list->head == NULL) {
+    list->head = new_node;
+  }else {
+    list->last->next = new_node;
+  }
+  list->count += 1;
+  list->last = new_node;
+  return Success;
+}
+
+void display(List_ptr list) {
+  Node_ptr p_walk = list->head;
+  while(p_walk != NULL)
+  {
+    printf("%d ", p_walk->value);
+    p_walk = p_walk->next;
+  }
+  printf("\n");
+}
--- numbers.c
+++ numbers.c
@@ -1,4 +1,45 @@
+#include <stdio.h>
+#include "list.h"
+
+void print_menu(void) {
+  printf("(a) add a number to the end of the list\n");
+  printf("(l) display the list of numbers\n");
+  printf("(m) exit\n");
+}
+
+Status perform_list_operation(List_ptr list, char menu) {
+  int num;
+  if (menu == 'a')
+  {
+    printf("enter a value\n");
+    scanf("%d",&num);
+    return add_to_end(list,num);
+  }
+  if (menu == 'l') {
+    display(list);
+    return Success;
+  }
+  return Failure;
+}
+
 int main(void)
 {
+  char menu;
+  Status status;
+  List_ptr list = create_list();
+
+  print_menu();
+  scanf("%c",&menu);
+
+  while (menu != 'm') {
+    status = perform_list_operation(list, menu);
+    // printf("%u\n",status);
+    if (status == Failure) {
+      printf("operation failed\n");
+    }
+    print_menu();
+    scanf("%c",&menu);
+  }
+  
   return 0;
 }
