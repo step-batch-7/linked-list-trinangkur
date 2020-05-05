38008ae4797fbd87e235a3cecaf235e4b43383a7
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2020-05-05 22:48:19 +0530

    Added unit test for list method add_to_end

---
+++ test/test.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <assert.h>
+#include "../list.h"
+
+void test_add_to_end(void) {
+  printf("testing add_to_end\n");
+  List_ptr list = create_list();
+  printf("\t should have one element while list is empty and have the value inserted\n");
+  assert(add_to_end(list,5));
+  assert(list->count == 1);
+  assert(list->head->value == 5);
+  printf("passed...\n");
+
+  printf("\t next of head element should be NULL while having one element\n");
+  assert(list->head->next == NULL);
+  printf("passed...\n");
+
+  printf("\t last element of list and head should be same while having one element\n");
+  assert(list->last == list->head);
+  printf("passed...\n");
+
+  printf("\t should have two element after inserting another element to the same list\n");
+  assert(add_to_end(list,3));
+  assert(list->count == 2);
+  assert(list->head->next->value == 3);
+  assert(list->last->value == 3);
+  printf("passed...\n");
+}
+
+int main(void)
+{
+  test_add_to_end();
+  return 0;
+}
